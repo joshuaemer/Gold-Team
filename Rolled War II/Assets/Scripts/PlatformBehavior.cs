ae2e74493e96e227a2a65e176673d9c2c10f5580
Josh <joshuaem@buffalo.edu>
2018-02-11 18:00:17 -0500

    Working Camera Script

---
+++ Rolled War II/Assets/Scripts/PlatformBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformBehavior : MonoBehaviour {
+    
+    private Rigidbody rb;
+    
+    public GameObject shaft;
+    private bool up;
+    //public Text test;
+    
+    private int speed;
+
+    // Use this for initialization
+    void Start () {
+        
+        rb = GetComponent<Rigidbody>();
+        up = false;
+        
+        speed = 5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (up)
+        {
+            
+            if (transform.position.y < shaft.transform.lossyScale.y)
+            {
+                rb.AddForce(new Vector3(0, speed, 0));
+            }
+            
+        }
+        else
+        {
+            up = false;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bottom Trigger")) {
+            if(transform.position.y == 0) {
+                up = true;
+            }
+        }
+    }
+
+}
---
+++ Rolled War II/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float move_horizontal = Input.GetAxis("Horizontal");
+        float move_vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(move_horizontal, 0, move_vertical);
+        rb.AddForce(movement * 10);
+    }
+
+}
