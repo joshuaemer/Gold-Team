f70eecb9db12f953845a7da3987c41383cd37edc
Josh <joshuaem@buffalo.edu>
2018-02-28 23:24:54 -0500

    Fixed Raycast hit

--- Rolled War II/Assets/Scripts/SkeletonMovement.cs
+++ Rolled War II/Assets/Scripts/SkeletonMovement.cs
@@ -10,7 +10,7 @@ using UnityEngine.AI;
 //Next need to add attack animation
 public class SkeletonMovement : MonoBehaviour {
     private Animator anim;
-    private float speed =5f;
+    private float speed =2f;
     private NavMeshAgent nav;
    
     //Check is an empty static game object that needs to have children that are also static and empty. The AI will follow go to each child to create a path.
@@ -19,53 +19,73 @@ public class SkeletonMovement : MonoBehaviour {
     
     public int direction;
     int speedHash;
+    int attackHash;
     int nextPoint = 0;
 
     //Raycasts Vars
     RaycastHit hit;
-    int lineOfSight = 15;
+    int lineOfSight = 20;
     bool foundPlayer = false;
     bool isFacing = false;
+    
     private GameObject target;
     // Use this for initialization
     void Start () {
         nav = GetComponent<NavMeshAgent>();
         speedHash = Animator.StringToHash("Speed");
+        attackHash = Animator.StringToHash("Attack");
         anim = GetComponent<Animator>();
         nav.speed = speed;
 
         
-        //anim.SetBool("isMoving",isMoving);
-        //anim.SetFloat("speed", speed);
+       
     }
 
     // Update is called once per frame
-    void LateUpdate () {
+    void Update () {
         //Checks if player is within line of sight if not move to the next checkpoint
         if (foundPlayer)
         {
-            if (inRange(2, target.transform.position)&& !isFacing)
+            if (inRange(2, target.transform.position))
             {
                 print("ATTACK");
-                transform.LookAt(target.transform);
-                isFacing = true;
-                
+                if (!isFacing)
+                {
+
+                    transform.LookAt(target.transform);
+                    isFacing = true;
+
+                }
+                else
+                {   //Order is important here, it needs to go from stand run state->stand->attack
+                    anim.SetFloat(speedHash, -1);
+
+                    anim.SetBool(attackHash, true);
+                    
+                }
             }
+
             else
             {
                 print("Locating");
                 nav.SetDestination(target.transform.position);
                 isFacing = false;
+
+                //Order is important here, it needs to go from stand run attack->stand->run
+                anim.SetBool(attackHash, false);
+                anim.SetFloat(speedHash, speed);
+                
             }
         }
         else
         {
-            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            if (Physics.Raycast(new Vector3(transform.position.x,transform.position.y+0.5f,transform.position.z), transform.forward, out hit,lineOfSight))
             {
-                
+                Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 3, transform.position.z), transform.forward.normalized * lineOfSight,Color.green);
 
-                if (hit.transform.CompareTag("Player") && inRange(lineOfSight, hit.transform.position))
+                if (hit.transform.CompareTag("Player"))
                 {
+                    print("hit");
                     foundPlayer = true;
                     target = hit.transform.gameObject;
                     nav.SetDestination(target.transform.position);
@@ -89,7 +109,7 @@ public class SkeletonMovement : MonoBehaviour {
                     }
                 }
 
-                anim.SetFloat(speedHash, 1);
+                anim.SetFloat(speedHash, speed);
                 nav.SetDestination(check.transform.GetChild(nextPoint).position);
 
             }
