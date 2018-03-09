3fea3a72066f526e238d003d08958563e3a6e2e9
Josh <joshuaem@buffalo.edu>
2018-03-09 16:55:01 -0500

    Manually merged fire rate

--- Rolled War II/Assets/Scripts/WeaponMechanics.cs
+++ Rolled War II/Assets/Scripts/WeaponMechanics.cs
@@ -5,7 +5,7 @@ using UnityEngine.Networking;
 
 public class WeaponMechanics : NetworkBehaviour {
 	private GameObject inv;
-	
+	private int curr= 0;
 	
 
 	// Use this for initialization
@@ -22,17 +22,63 @@ public class WeaponMechanics : NetworkBehaviour {
         if (!hasAuthority) { return; }
 		// If the left mouse button is clicked, fire.
 		if (Input.GetMouseButton(0)) {
-			// TODO
-			shoot();
-
+            if (Input.GetMouseButtonDown(0))
+            {
+                // TODO
+                if (curr == 0)
+                {
+                    waitForNSeconds(0.4f);
+                    shoot();
+                }
+                else if (curr == 1)
+                {
+                    waitForNSeconds(2);
+                    shoot();
+                }
+                else if (curr == 2)
+                {
+                    //yield return new WaitForSeconds(4);
+                    waitForNSeconds(4);
+                    shoot();
+                }
+                else if (curr == 3)
+                {
+                    waitForNSeconds(0.05f);
+                    shoot();
+                }
+                else if (curr == 4)
+                {
+                    waitForNSeconds(0.05f);
+                    shoot();
+                }
+                else if (curr == 5)
+                {
+                    //yield return new WaitForSeconds(3);
+                    waitForNSeconds(3);
+                }
+                else
+                {
+                    Debug.LogError("ERR: Invalid curr value: " + curr + ".  Quitting....");
+                    Application.Quit();
+                }
+            }
 		}
 		else if (Input.GetKeyDown("e")) {
             //Switch Weapons
-			inv.GetComponent<InventorySystem>().switchWeapon();
+			curr = inv.GetComponent<InventorySystem>().switchWeapon();
 		}
 	}
+    
 
-	
+	/*
+	waitForNSeconds(float n): Function that takes in a float argument called n, and delays the function that calls this one
+	by that number n in seconds.  For example, n = 0.25f means delay by a quarter of a second, and n=4
+	means delay by 4 seconds.  This essentially puts a lower bound on the time that would be necessary
+	wait before a mouse click (fire) would register.
+	 */
+	IEnumerator waitForNSeconds(float n) {
+		yield return new WaitForSeconds(n);
+	}
 
 
 	void shoot() {
