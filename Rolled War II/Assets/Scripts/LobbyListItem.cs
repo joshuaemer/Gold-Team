42da39f44a02056b0ae3ba20fb58c035fe3eb81b
mjholder <mjholder96@gmail.com>
2018-04-04 08:21:36 -0400

    New Networking System
    Now uses unity match making service. also has support for creating and joining lobbies. each lobby has max player limit of 4

---
+++ Rolled War II/Assets/Scripts/LobbyListItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking.Match;
+
+public class LobbyListItem : MonoBehaviour {
+
+    public delegate void JoinRoomDelegate(MatchInfoSnapshot _match);
+    private JoinRoomDelegate joinRoomCallback;
+
+    [SerializeField]
+    private Text lobbyNameText;
+
+    private MatchInfoSnapshot match;
+
+    public void Setup(MatchInfoSnapshot _match, JoinRoomDelegate _joinRoomCallback) {
+        match = _match;
+        joinRoomCallback = _joinRoomCallback;
+
+        lobbyNameText.text = match.name + " (" + match.currentSize + "/" + match.maxSize + ")";
+    }
+
+    public void JoinRoom() {
+        joinRoomCallback.Invoke(match);
+    }
+}
--- Rolled War II/Assets/Scripts/MenuController.cs
+++ Rolled War II/Assets/Scripts/MenuController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
 using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour {
+
+    private uint lobbySize = 4;
+    private string lobbyName;
 
     private bool inGame = false;
     private bool toggle = false;
@@ -14,36 +18,104 @@ public class MenuController : MonoBehaviour {
     public GameObject controllsMenu;
     public GameObject gameOver;
     public Text score;
+    public GameObject lobbyListItemPrefab;
+    public Transform lobbyListParent;
+    private NetworkManager manager;
+
+    private List<GameObject> lobbyList = new List<GameObject>();
+
+    [SerializeField]
+    private Text status;
 
     GameManager gm;
 
-    // Host a Game
-	public void HostTDM(NetworkManager manager) {
-        manager.StartHost();
-        gm = GameObject.FindObjectOfType<GameManager>();
-        gm.SetGameType("TDM");
-        inGame = true;
-        mainMenu.SetActive(false);
+    void Start() {
+        manager = NetworkManager.singleton;
+        if(manager.matchMaker == null) {
+            manager.StartMatchMaker();
+        }
+
+        RefreshLobbies();
     }
 
-    public void HostFFA(NetworkManager manager) {
-        manager.StartHost();
-        gm = GameObject.FindObjectOfType<GameManager>();
-        gm.SetGameType("FFA");
-        inGame = true;
-        mainMenu.SetActive(false);
+    public void RefreshLobbies() {
+        ClearLobbyList();
+        manager.matchMaker.ListMatches(0, 20, "", false, 0, 0, OnMatchList);
+        status.text = "Loading....";
     }
 
-    // Try to connect to a hosted game
-    public void Connect(NetworkManager manager) {
-        gm = GameObject.FindObjectOfType<GameManager>();
-        NetworkClient client = manager.StartClient();
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches) {
+        status.text = "";
+        if(!success || matches == null) {
+            status.text = "Couldn't get lobby list.";
+            return;
+        }
+
+        foreach(MatchInfoSnapshot match in matches) {
+            GameObject lobbyListItemGO = Instantiate(lobbyListItemPrefab);
+            lobbyListItemGO.transform.SetParent(lobbyListParent);
+
+            LobbyListItem lobbyListItem = lobbyListItemGO.GetComponent<LobbyListItem>();
+            if (lobbyListItem != null) {
+                lobbyListItem.Setup(match, JoinRoom);
+            }
+
+
+            // as well as setting up a callback function that will join the game.
+
+            lobbyList.Add(lobbyListItemGO);
+        }
+
+        if (lobbyList.Count == 0) {
+            status.text = "No rooms at the moment.";
+        }
     }
 
-    // When the player gets connected it calls this function
-    public void ConfirmConnect() {
-        inGame = true;
-        mainMenu.SetActive(false);
+    void ClearLobbyList() {
+        for(int i = 0; i < lobbyList.Count; i++) {
+            Destroy(lobbyList[i]);
+        }
+        lobbyList.Clear();
+    }
+
+    public void SetLobbyName(string name) {
+        lobbyName = name;
+    }
+
+    public void CreateLobby() {
+        if (lobbyName != "" && lobbyName != null) {
+            manager.matchMaker.CreateMatch(lobbyName, lobbySize, true, "", "", "", 0, 0, manager.OnMatchCreate);
+        }
+    }
+
+    public void JoinRoom(MatchInfoSnapshot _match) {
+        manager.matchMaker.JoinMatch(_match.networkId, "", "", "", 0, 0, manager.OnMatchJoined);
+        StartCoroutine(WaitForJoin());
+    }
+
+    IEnumerator WaitForJoin() {
+        ClearLobbyList();
+
+        int countdown = 5;
+        while (countdown > 0) {
+            status.text = "JOINING... (" + countdown + ")";
+
+            yield return new WaitForSeconds(1);
+
+            countdown--;
+        }
+        
+        status.text = "Failed to connect.";
+        yield return new WaitForSeconds(1);
+
+        MatchInfo matchInfo = manager.matchInfo;
+        if (matchInfo != null) {
+            manager.matchMaker.DropConnection(matchInfo.networkId, matchInfo.nodeId, 0, manager.OnDropConnection);
+            manager.StopHost();
+        }
+
+        RefreshLobbies();
+
     }
 
     // Quit the Application
@@ -52,13 +124,10 @@ public class MenuController : MonoBehaviour {
     }
 
     // Leave game or Stop Hosting a game
-    public void Disconnect(NetworkManager manager) {
-        try {
-            manager.StopHost();
-        }
-        catch {
-            manager.StopClient();
-        }
+    public void Disconnect() {
+        MatchInfo matchInfo = manager.matchInfo;
+        manager.matchMaker.DropConnection(matchInfo.networkId, matchInfo.nodeId, 0, manager.OnDropConnection);
+        manager.StopHost();
         inGame = false;
         playerMenu.SetActive(false);
         mainCamera.transform.SetPositionAndRotation(new Vector3(0f, 50f, 0f), Quaternion.Euler(90, 0, 0));
